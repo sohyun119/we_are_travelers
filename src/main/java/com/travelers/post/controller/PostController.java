f8d2c19f244ddeb59b652c2cc0ba03cd23b41195
Sohyun <sh-119@naver.com>
2024-01-24 17:01:33 +0900

    [ We are Travelers ] 1. 기본 셋팅 2. home 프론트 설정중

---
+++ src/main/java/com/travelers/post/controller/PostController.java
@@ -0,0 +1,14 @@
+package com.travelers.post.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PostController {
+	
+	@GetMapping("/test")
+	public String test() {
+		return "post/subscribePost";
+	}
+
+}
